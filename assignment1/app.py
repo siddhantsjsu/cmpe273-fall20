31d1b11db7c037f28336fe2eeb9f85801b039806
Siddhant <siddhant.sribhashyam@sjsu.edu>
2020-09-23 16:01:52 -0700

    Added APIs for Delete, QRCode and Stats

--- assignment1/app.py
+++ assignment1/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, send_file, make_response
 from sqlitedict import SqliteDict
-import hashlib
+import hashlib 
+import qrcode
+from io import BytesIO
 bookmarksDB = SqliteDict('./my_db.sqlite')
 
 app = Flask(__name__)
@@ -14,7 +16,6 @@ def createBookmark():
         return resp, 400
     else:
         reqDict = request.get_json()
-        # print(reqDict)
         reqDict['readCount'] = 0
         bookmarksDB[hashedUrl] = reqDict
         bookmarksDB.commit()
@@ -27,13 +28,53 @@ def getBookmark(id):
         abort(404)
     else:
         resp = bookmarksDB.get(id)
-        # print(resp)
         resp['readCount'] += 1
+        ETag = hashlib.md5((resp['name'] + resp['url'] + str(resp['readCount'])).encode("utf-8")).hexdigest()
+        resp['ETag'] = ETag
         bookmarksDB[id] = resp
         bookmarksDB.commit()
         resp.pop('readCount')
-        # print(bookmarksDB.get(id))
+        resp.pop('ETag')
         return resp
         
-    
+@app.route('/api/bookmarks/<id>', methods = ['DELETE'])
+def deleteBookmark(id):
+    if id not in bookmarksDB:
+        abort(404)
+    else:
+        bookmarksDB.pop(id)
+        bookmarksDB.commit()
+        return '', 204
 
+@app.route('/api/bookmarks/<id>/qrcode', methods = ['GET'])
+def getBookmarkQRcode(id):
+    if id not in bookmarksDB:
+        abort(404)
+    else:
+        bookmark = bookmarksDB.get(id)
+        img = qrcode.make(bookmark.get('url'))
+        buf = BytesIO()
+        img.save(buf, format = 'JPEG')
+        buf.seek(0)
+        return send_file(buf, mimetype="image/jpeg", attachment_filename="qrcode.jpg", as_attachment=True)
+
+@app.route('/api/bookmarks/<id>/stats', methods = ['GET'])
+def getBookmarkStats(id):
+    if id not in bookmarksDB:
+        abort(404)
+    else:
+        if request.headers.get('ETag') == None or request.headers.get('ETag') == '':
+            bookmark = bookmarksDB.get(id)
+            resp = make_response(str(bookmark.get('readCount')), 200)
+            resp.headers['ETag'] = bookmark.get('ETag')
+            return resp
+        else:
+            bookmark = bookmarksDB.get(id)
+            if request.headers.get('ETag') == bookmark.get('ETag'):
+                resp = make_response('', 304)
+                resp.headers['ETag'] = bookmark.get('ETag')
+                return resp
+            else:
+                resp = make_response(str(bookmark.get('readCount')), 200)
+                resp.headers['ETag'] = bookmark.get('ETag')
+                return resp
